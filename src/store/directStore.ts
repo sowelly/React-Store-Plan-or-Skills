a81de024d90d89e83bf0da54c4a0746628913b96
ann <annjesson23@gmail.com>
2025-11-25 16:33:01 +0800

    build: 移除构建命令中的tsc步骤

--- src/store/directStore.ts
+++ src/store/directStore.ts
@@ -7,7 +7,7 @@ type DirectState = {
   incY: () => void
 }
 
-export const useDirectStore = create<DirectState>()((set, get) => ({
+export const useDirectStore = create<DirectState>()((set) => ({
   x: 0,
   y: 0,
   incX: () => set((s) => ({ x: s.x + 1 })),
